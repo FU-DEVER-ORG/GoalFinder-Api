080faccacd7343ff3d89e03bfb8fede2417dff26
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2024-04-18 23:10:06 +0700

    Init_Persistence_Module-C3: Adding base repository and base code of unit of work.

---
+++ src/External/Persistence/Relational/GoalFinder.MySqlRelationalDb/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,46 @@
+﻿using GoalFinder.Data.Entities.Base;
+using GoalFinder.Data.Repositories.Base;
+using GoalFinder.MySqlRelationalDb.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoalFinder.MySqlRelationalDb.Repositories.Base;
+
+/// <summary>
+///     Implementation of base repository.
+/// </summary>
+/// <typeparam name="TEntity">
+///     Entity type.
+/// </typeparam>
+internal abstract class BaseRepository<TEntity> : IBaseRepository<TEntity>
+    where TEntity :
+        class,
+        IBaseEntity
+{
+    protected readonly DbSet<TEntity> _dbSet;
+
+    private protected BaseRepository(GoalFinderContext context)
+    {
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task AddAsync(
+        TEntity newEntity,
+        CancellationToken cancellationToken)
+    {
+        await _dbSet.AddAsync(
+            entity: newEntity,
+            cancellationToken: cancellationToken);
+    }
+
+    public Task AddRangeAsync(
+        IEnumerable<TEntity> newEntities,
+        CancellationToken cancellationToken)
+    {
+        return _dbSet.AddRangeAsync(
+            entities: newEntities,
+            cancellationToken: cancellationToken);
+    }
+}
---
+++ src/External/Persistence/Relational/GoalFinder.MySqlRelationalDb/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using GoalFinder.Data.UnitOfWork;
+
+namespace GoalFinder.MySqlRelationalDb.UnitOfWork;
+
+/// <summary>
+///     Implementation of unit of work interface.
+/// </summary>
+internal sealed class UnitOfWork : IUnitOfWork
+{
+}
