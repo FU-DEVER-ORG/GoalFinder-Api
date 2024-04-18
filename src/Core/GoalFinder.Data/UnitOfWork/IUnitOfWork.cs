4e4dd847aac1e7e3fe386da683f15d2acd100aa2
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2024-04-18 23:14:52 +0700

    Init_Persistence_Module-C4: Adding database transaction apis to unit of work.

--- src/Core/GoalFinder.Data/UnitOfWork/IUnitOfWork.cs
+++ src/Core/GoalFinder.Data/UnitOfWork/IUnitOfWork.cs
@@ -1,8 +1,80 @@
-﻿namespace GoalFinder.Data.UnitOfWork;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GoalFinder.Data.UnitOfWork;
 
 /// <summary>
 ///     Interface for unit of work.
 /// </summary>
 public interface IUnitOfWork
 {
+    /// <summary>
+    ///     Create an execution strategy for managing the
+    ///     db transaction which is initialized inside.
+    /// </summary>
+    /// <returns>
+    ///     IExecutionStrategy for wrapping transaction inside.
+    /// </returns>
+    IExecutionStrategy CreateExecutionStrategy();
+
+    /// <summary>
+    ///     Create a database transaction.
+    /// </summary>
+    /// <param name="cancellationToken">
+    ///     A token that is used for notifying system
+    ///     to cancel the current operation when user stop
+    ///     the request.
+    /// </param>
+    /// <returns>
+    ///     A task containing result of operation.
+    /// </returns>
+    Task CreateTransactionAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    ///     Commit a database transaction.
+    /// </summary>
+    /// <param name="cancellationToken">
+    ///     A token that is used for notifying system
+    ///     to cancel the current operation when user stop
+    ///     the request.
+    /// </param>
+    /// <returns>
+    ///     A task containing result of operation.
+    /// </returns>
+    Task CommitTransactionAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    ///     Rollback the database transaction.
+    /// </summary>
+    /// <param name="cancellationToken">
+    ///     A token that is used for notifying system
+    ///     to cancel the current operation when user stop
+    ///     the request.
+    /// </param>
+    /// <returns>
+    ///     A task containing result of operation.
+    /// </returns>
+    Task RollBackTransactionAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    ///     Dispose the database transaction after done using
+    /// </summary>
+    /// <returns>
+    ///     A value task containing result of operation.
+    /// </returns>
+    ValueTask DisposeTransactionAsync();
+
+    /// <summary>
+    ///     Save all entity changes to the database.
+    /// </summary>
+    /// <param name="cancellationToken">
+    ///     A token that is used for notifying system
+    ///     to cancel the current operation when user stop
+    ///     the request.
+    /// </param>
+    /// <returns>
+    ///     A task containing result of operation.
+    /// </returns>
+    Task SaveToDatabaseAsync(CancellationToken cancellationToken);
 }
--- src/External/Persistence/Relational/GoalFinder.MySqlRelationalDb/UnitOfWork/UnitOfWork.cs
+++ src/External/Persistence/Relational/GoalFinder.MySqlRelationalDb/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,8 @@
 ﻿using GoalFinder.Data.UnitOfWork;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading.Tasks;
+using System.Threading;
+using GoalFinder.MySqlRelationalDb.Data;
 
 namespace GoalFinder.MySqlRelationalDb.UnitOfWork;
 
@@ -7,4 +11,40 @@ namespace GoalFinder.MySqlRelationalDb.UnitOfWork;
 /// </summary>
 internal sealed class UnitOfWork : IUnitOfWork
 {
+    private readonly GoalFinderContext _context;
+    private IDbContextTransaction _dbTransaction;
+
+    public UnitOfWork(GoalFinderContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateTransactionAsync(CancellationToken cancellationToken)
+    {
+        _dbTransaction = await _context.Database.BeginTransactionAsync(cancellationToken: cancellationToken);
+    }
+
+    public Task CommitTransactionAsync(CancellationToken cancellationToken)
+    {
+        return _dbTransaction.CommitAsync(cancellationToken: cancellationToken);
+    }
+
+    public Task RollBackTransactionAsync(CancellationToken cancellationToken)
+    {
+        return _dbTransaction.RollbackAsync(cancellationToken: cancellationToken);
+    }
+
+    public ValueTask DisposeTransactionAsync()
+    {
+        return _dbTransaction.DisposeAsync();
+    }
+
+    public IExecutionStrategy CreateExecutionStrategy()
+    {
+        return _context.Database.CreateExecutionStrategy();
+    }
+    public Task SaveToDatabaseAsync(CancellationToken cancellationToken)
+    {
+        return _context.SaveChangesAsync(cancellationToken: cancellationToken);
+    }
 }
